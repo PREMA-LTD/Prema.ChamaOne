3db5e4ac97563ea3d44f6a325295b66c7c04c1ba
Enock Kimutai <92427438+enock-kim@users.noreply.github.com>
2024-07-16 19:21:19 +0300

    finally got frontend workig

--- Prema.ChamaOne.Frontend/src/app/app.component.ts
+++ Prema.ChamaOne.Frontend/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, inject } from '@angular/core';
+import { PreloaderService, SettingsService } from '@core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `<router-outlet />`,
 })
-export class AppComponent {
-  title = 'Prema.ChamaOne.Frontend';
+export class AppComponent implements OnInit, AfterViewInit {
+  private readonly preloader = inject(PreloaderService);
+  private readonly settings = inject(SettingsService);
+
+  ngOnInit() {
+    this.settings.setDirection();
+    this.settings.setTheme();
+  }
+
+  ngAfterViewInit() {
+    this.preloader.hide();
+  }
 }
