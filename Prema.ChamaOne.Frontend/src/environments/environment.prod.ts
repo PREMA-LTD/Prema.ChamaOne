0767a676c0594469da8d435a66021364b983442e
Enock Kimutai <92427438+enock-kim@users.noreply.github.com>
2024-07-23 13:38:51 +0000

    changed api url

--- Prema.ChamaOne.Frontend/src/environments/environment.prod.ts
+++ Prema.ChamaOne.Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://chamaone.prema.co.ke/api',
+  apiUrl: 'https://fintrack-backend.shangilia.africa/api',
   baseUrl: '',
   useHash: false,
 };
