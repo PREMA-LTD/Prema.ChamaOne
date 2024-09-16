9f01b38e80248b229858e5d09887b87a5b8c32c1
Enock Kimutai <92427438+enock-kim@users.noreply.github.com>
2024-09-16 12:53:52 +0300

    updated action scripts and added live url

--- Prema.ChamaOne.Frontend/src/environments/environment.prod.ts
+++ Prema.ChamaOne.Frontend/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'https://fintrack-backend.shangilia.africa/api',
   baseUrl: '',
   useHash: false,
-  keycloakUrl: 'https://keycloak-prod.prema.co.ke/realms/chama-one/protocol/openid-connect/auth'
+  keycloakUrl: 'https://keycloak-prod.prema.co.ke'
 };
