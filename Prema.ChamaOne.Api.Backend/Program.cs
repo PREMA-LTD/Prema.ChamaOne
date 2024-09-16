e6b3f445705ed6a0ce4b22e7f00a198968e7ef41
Enock Kimutai <92427438+enock-kim@users.noreply.github.com>
2024-09-16 15:12:56 +0300

    updated settings

--- Prema.ChamaOne.Api.Backend/Program.cs
+++ Prema.ChamaOne.Api.Backend/Program.cs
@@ -79,7 +79,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ChamaOneDatabaseContext>();
-    //dbContext.Database.Migrate();
+    dbContext.Database.Migrate();
 }
 
 // Configure the HTTP request pipeline.
