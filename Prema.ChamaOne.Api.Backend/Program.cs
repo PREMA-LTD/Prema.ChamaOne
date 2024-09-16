ba18867083dfbd0a03568bfe590c3b1b82f697a4
Enock Kimutai <92427438+enock-kim@users.noreply.github.com>
2024-09-16 16:40:22 +0300

    trying sweagger

--- Prema.ChamaOne.Api.Backend/Program.cs
+++ Prema.ChamaOne.Api.Backend/Program.cs
@@ -79,15 +79,12 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ChamaOneDatabaseContext>();
-    dbContext.Database.Migrate();
+    //dbContext.Database.Migrate();
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
