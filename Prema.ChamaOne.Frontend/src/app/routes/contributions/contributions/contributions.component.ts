889d73b4ad4f1378878e287c547f35d2b3808665
Enock Kimutai <92427438+enock-kim@users.noreply.github.com>
2024-09-18 17:07:34 +0300

    updated exception

--- Prema.ChamaOne.Frontend/src/app/routes/contributions/contributions/contributions.component.ts
+++ Prema.ChamaOne.Frontend/src/app/routes/contributions/contributions/contributions.component.ts
@@ -64,7 +64,7 @@ export class ContributionsContributionsComponent implements OnInit {
         {
           text: 'Pay',
           color: 'primary',
-          iif: (record: any) => record.fk_transaction_status_id !== 1 && !this.keycloakService.isUserInRole("member"),
+          iif: (record: any) => record.fk_transaction_status_id !== 1 && this.keycloakService.isUserInRole("admin"),
           click: (record: any) => this.openPayModal(record)
         }
       ]
